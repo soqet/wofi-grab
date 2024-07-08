42668a4400c3286bb9fc5d526924d2ad33e46aba
Dmitry <soqet@bk.ru>
2024-07-08 22:19:30 +0700

    added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wofi window switcher
+
+Plugin for wofi written in zig.
+
+## Usage
+
+```
+git clone https://github.com/soqet/wofi-grab.git
+cd wofi-grab
+zig build
+mv zig-out/lib/libwofi-grab.so ~/.config/wofi/plugins/wg.so
+bash grab_window.sh
+```
+
+
--- grab_window.sh
+++ grab_window.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! $(pidof wofi) ]]; then
-    ADDRESS="$( hyprctl clients -j |  wofi  --style  ~/.config/wofi/styles.css --show wg.so )"
+    ADDRESS="$( hyprctl clients -j |  wofi  --style  ~/.config/wofi/style.css --show wg.so )"
     if [ -n "$ADDRESS" ]; then
         hyprctl dispatch movetoworkspace $( hyprctl activeworkspace -j | jq .id ),address:"$ADDRESS"
     fi    
